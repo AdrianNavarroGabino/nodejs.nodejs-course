5d458a7df80f390078a4e8a6b9b03ca37197f44e
Adrian Navarro Gabino <adriannavarrogabino@gmail.com>
2022-02-09 22:38:06 +0100

    Const vs Let vs Var

---
+++ 02-fundamentos/const-var-let.js
@@ -0,0 +1,67 @@
+var nombre1 = "Wolverine";
+
+if(true) {
+    var nombre1 = "Magneto";
+
+    console.log(nombre1);
+}
+
+console.log(nombre1);
+console.log("-----------------");
+
+let nombre2 = "Wolverine";
+
+if(true) {
+    let nombre2 = "Magneto";
+
+    console.log(nombre2);
+}
+
+console.log(nombre2);
+console.log("-----------------");
+
+let nombre3 = "Wolverine";
+
+if(true) {
+    nombre3 = "Magneto";
+
+    console.log(nombre3);
+}
+
+console.log(nombre3);
+console.log("-----------------");
+
+const nombre4 = "Wolverine";
+
+if(true) {
+    try {
+        nombre4 = "Magneto";
+    }
+    catch(error) {}
+}
+
+console.log(nombre4);
+console.log("-----------------");
+
+const nombre5 = "Wolverine";
+
+if(true) {
+    try {
+        const nombre5 = "Magneto";
+    }
+    catch(error) {}
+    console.log(nombre5);
+}
+
+console.log(nombre5);
+console.log("-----------------");
+
+nombre6 = "Wolverine";
+
+if(true) {
+    nombre6 = "Magneto";
+}
+
+var nombre6;
+
+console.log(nombre6);
