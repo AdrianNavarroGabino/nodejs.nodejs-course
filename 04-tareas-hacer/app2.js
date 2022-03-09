bdaae4cf2119f5462af451b0c9bdd734d3402b4a
Adrian Navarro Gabino <adriannavarrogabino@gmail.com>
2022-03-09 22:09:19 +0100

    Lógica para el manejo de las tareas por hacer

--- 04-tareas-hacer/app2.js
+++ 04-tareas-hacer/app2.js
@@ -1,5 +1,7 @@
 require("colors");
 const { inquirerMenu, pausa } = require("./helpers/inquirer");
+const Tarea = require("./models/tarea");
+const Tareas = require("./models/tareas");
 
 
 console.clear();
@@ -10,8 +12,16 @@ const main = async() => {
     let opt = '';
 
     do {
-        opt = await inquirerMenu();
-        
+        //opt = await inquirerMenu();
+
+        const tarea = new Tarea('Comprar comida');
+        console.log(tarea);
+
+        const tareas = new Tareas();
+
+        tareas._listado[tarea.id] = tarea;
+
+        console.log(tareas);
         if(opt != '0')
             await pausa();
     } while(opt !== '0');
